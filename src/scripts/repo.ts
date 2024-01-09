ef3856fe0413dec49eebdde7cf53b61587aa7959
Damian Aldair <daldairgp@gmail.com>
2024-01-09 17:13:19 -0500

    Experience section style

--- src/scripts/repo.ts
+++ src/scripts/repo.ts
@@ -1,5 +1,3 @@
-
-
 export interface Skill {
     name: string;
     r: number;
@@ -25,4 +23,45 @@ export function getImgFromSkill(skill: string): string {
         .toLowerCase()
         .replaceAll(' ', '_')
         + '.svg';
+}
+
+export interface Experience {
+    startYear: number;
+    finishYear: number;
+    title: string;
+    description: string;
+    url?: string;
+}
+
+export const experiences: Experience[] = [
+    {
+        startYear: 2021,
+        finishYear: 2023,
+        title: 'Desarrollador móvil como estudiante de la CUJAE',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque id libero dicta tempora, adipisci, quis eos, saepe perspiciatis quos illum ipsum ea sed fugiat repellendus odit commodi illo tenetur quod?',
+        url: 'https://www.redalyc.org/journal/5600/560059292002/html/',
+    },
+    {
+        startYear: 2024,
+        finishYear: 2024,
+        title: 'Trabajo para un chino',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque id libero dicta tempora, adipisci, quis eos, saepe perspiciatis quos illum ipsum ea sed fugiat repellendus odit commodi illo tenetur quod?'
+    },
+    {
+        startYear: 2023,
+        finishYear: -1,
+        title: 'Confeción de mi porfolio',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque id libero dicta tempora, adipisci, quis eos, saepe perspiciatis quos illum ipsum ea sed fugiat repellendus odit commodi illo tenetur quod?'
+    },
+];
+
+export function getExperienceDate(experience: Experience): string {
+    const { startYear: y1, finishYear: y2 } = experience;
+
+    if (y2 === y1) return `${y1}`;
+    if (y2 != -1) return `${y1} - ${y2}`;
+
+    const currentYear = new Date().getFullYear();
+    if (y1 != currentYear) return `${y1} - Actualidad`;
+    return 'Actualmente';
 }
